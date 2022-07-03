63b5dbe924c52587fc5fa190390e78fa7a3dae23
ajy9844 <ajy09844@gmail.com>
2022-07-03 16:00:46 +0900

    feat : add member web function

--- spring-intro/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ spring-intro/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -19,4 +19,26 @@ public class MemberController {
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
 }
